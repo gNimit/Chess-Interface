7c1a8c397652722be2fcba1975b2a47726450690
Nimit Gaur <73322773+gNimit@users.noreply.github.com>
2022-04-15 22:46:12 +0530

    Build

--- chessboard/src/app/app.module.ts
+++ chessboard/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
 import { KnightComponent } from './knight/knight.component';
 import { GameServiceService } from './game-service.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -18,7 +20,13 @@ import { GameServiceService } from './game-service.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [GameServiceService],
   bootstrap: [AppComponent]
